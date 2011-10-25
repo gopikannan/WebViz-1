c55e14d2615051630be308a4034745253095ca4a
Gopikannan <gopi21489@gmail.com>
2011-10-25 01:09:42 -0400

    1.0

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -1,44 +1,10 @@
 class ClicksController < ApplicationController
   def create
 	  if session[:latest_click] < 0  	
-	  	@click = Click.new
-         	@click.poll_id = params[:poll_id]
-          	@click.user_id = current_user.id
-          	@click.option = params[:option]
-          	@click.location = params[:loc]
-          	@click.save
-         	session[:latest_click]=Time.now.to_i
-		@country_click = CountryClicks.where(:poll_id =>params[:poll_id],:date => get_date,:country =>params[:country]) || CountryClicks.create_with_params(params,get_date)
-		 @country_click.each do|var| 
-			if var !=nil
-				if(params[:option] ==1)
-					var.clicks+=1
-				else
-					var.clicks-=1	
-				end
-				var.save
-			end
-		end
+	  	storeclicks(params)
 	  else
 	 	if Time.now.to_i - session[:latest_click] > 60	 
-	  		@click = Click.new
-         		@click.poll_id = params[:poll_id]
-          		@click.user_id = current_user.id
-          		@click.option = params[:option]
-          		@click.location = params[:loc]
-          		@click.save
-			session[:latest_click]=Time.now.to_i
-			@country_click = CountryClicks.where(:poll_id =>params[:poll_id],:date => get_date,:country => params[:country]) || CountryClicks.where(:poll_id =>params[:poll_id],:date => get_date) 
-		 	@country_click.each do|var| 
-				if var !=nil
-					if(params[:option]==1)
-						var.clicks+=1
-					else
-						var.clicks-=1
-					end
-					var.save
-				end
-			end
+			storeclicks(params)
 		else
 			respond_to do |format|  
       				format.html {   
@@ -62,5 +28,27 @@ class ClicksController < ApplicationController
 		format.js{render:json => @countryclicks.to_json}
 	end 
   end
+
+  def storeclicks(params)
+	 @click = Click.new
+         @click.poll_id = params[:poll_id]
+         @click.user_id = current_user.id
+         @click.option = params[:option]
+         @click.location = params[:loc]
+         @click.save
+         session[:latest_click]=Time.now.to_i
+	 @country_click = CountryClicks.where(:poll_id =>params[:poll_id],:date => get_date,:country =>params[:country]) || CountryClicks.create_with_params(params,get_date)
+	 @country_click.each do|var| 
+	 	if var !=nil
+			if(params[:option] ==1)
+				var.clicks+=1
+			else
+				var.clicks-=1	
+			end
+			var.save
+		end
+	end
+  
+  end 
 
 end
